1fb8306673f239503e2c9dc0858b38c2386509b6
gangmidieo-beep <gangmidieo@gmail.com>
2026-08-30 13:40:17 +0900

    Delete lib directory

--- lib/site-config.ts
+++
@@ -1,8 +0,0 @@
-/** 바로산지 카카오톡 채널 상담 링크 */
-export const KAKAO_CHANNEL_URL = "https://pf.kakao.com/_cHuxaX/friend";
-
-/** 배송비 정책 — 주문 금액과 무관하게 건당 고정 배송비 */
-export const SHIPPING_FEE = 4000;
-
-/** 상품별 1인당 최대 구매 수량 (복수 품목 구매를 유도하기 위한 기본값, 상품별로 다르게 지정 가능) */
-export const DEFAULT_MAX_QTY_PER_PRODUCT = 5;
