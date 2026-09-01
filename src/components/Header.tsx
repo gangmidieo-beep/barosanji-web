b432b75ec381c201b70e9616e4bfdcd22e2f5f4a
gangmidieo-beep <gangmidieo@gmail.com>
2026-09-01 10:32:43 +0900

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,6 @@ import Link from "next/link";
 import Image from "next/image";
 import { useState } from "react";
 import { useRouter } from "next/navigation";
-import { categories } from "@/lib/data";
 
 export default function Header() {
   const [query, setQuery] = useState("");
